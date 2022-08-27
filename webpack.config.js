40cb6ea1647a09aeeed2b1afe557c3fc918a2869
MiguelGDD <miguel.guell@unisimon.edu.co>
2022-08-27 15:21:33 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  output: { filename: 'app.bundle.js' },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development'
+}
